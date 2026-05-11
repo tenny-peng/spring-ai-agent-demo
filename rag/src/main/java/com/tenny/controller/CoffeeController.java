a99aedaab2bbe53880a98b1058e53608221c536e
tenny <mpengtaoqi@gmail.com>
2026-05-11 09:24:44 +0800

    rag advisor demo

--- rag/src/main/java/com/tenny/controller/CoffeeController.java
+++ rag/src/main/java/com/tenny/controller/CoffeeController.java
@@ -3,11 +3,16 @@ package com.tenny.controller;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.DocumentRetriever;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -22,8 +27,23 @@ public class CoffeeController {
 
     private final VectorStore vectorStore;
 
-    public CoffeeController(VectorStore vectorStore) {
+    private final ChatClient chatClient;
+
+    public CoffeeController(VectorStore vectorStore, ChatClient.Builder clientBuilder) {
         this.vectorStore = vectorStore;
+
+        VectorStoreDocumentRetriever vectorStoreDocumentRetriever = VectorStoreDocumentRetriever
+                .builder()
+                .vectorStore(this.vectorStore)
+                .build();
+        RetrievalAugmentationAdvisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor
+                .builder()
+                .documentRetriever(vectorStoreDocumentRetriever)
+                .build();
+
+        this.chatClient = clientBuilder
+                .defaultAdvisors(retrievalAugmentationAdvisor)
+                .build();
     }
 
     @GetMapping("importData")
@@ -59,6 +79,15 @@ public class CoffeeController {
         } catch (IOException e) {
             return("加载 CSV 文件失败: " + e.getMessage());
         }
+    }
+
+    @GetMapping("rag-ask")
+    public String ragAsk(@RequestParam("question") String question){
+        return chatClient.prompt()
+                .system("你是一个咖啡店的服务员，你需要回答用户的问题")
+                .user(question)
+                .call()
+                .content();
     }
 
 }
