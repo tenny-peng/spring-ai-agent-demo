9a6437c936a263e437f5b41b75fa6d7f275b989d
tenny <mpengtaoqi@gmail.com>
2026-05-20 08:32:02 +0800

    graph demo

---
+++ graph/src/main/java/com/tenny/config/GraphConfig.java
@@ -0,0 +1,54 @@
+package com.tenny.config;
+
+import com.alibaba.cloud.ai.graph.*;
+import com.alibaba.cloud.ai.graph.action.AsyncNodeAction;
+import com.alibaba.cloud.ai.graph.action.NodeAction;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.alibaba.cloud.ai.graph.state.strategy.ReplaceStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class GraphConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(GraphConfig.class);
+
+    @Bean("quickStartGraph")
+    public CompiledGraph quickStartGraph() throws GraphStateException {
+        KeyStrategyFactory keyStrategyFactory = new KeyStrategyFactory() {
+            @Override
+            public Map<String, KeyStrategy> apply() {
+                return Map.of("input1", new ReplaceStrategy(),
+                        "input2", new ReplaceStrategy());
+            }
+        };
+
+        StateGraph stateGraph = new StateGraph("quickStartGraph", keyStrategyFactory);
+
+        stateGraph.addNode("node1", AsyncNodeAction.node_async(new NodeAction() {
+            @Override
+            public Map<String, Object> apply(OverAllState state) throws Exception {
+                log.info("node1 state:{}", state);
+                return Map.of("input1", 1, "input2", 1);
+            }
+        }));
+
+        stateGraph.addNode("node2", AsyncNodeAction.node_async(new NodeAction() {
+            @Override
+            public Map<String, Object> apply(OverAllState state) throws Exception {
+                log.info("node2 state:{}", state);
+                return Map.of("input1", 2, "input2", 2);
+            }
+        }));
+
+        stateGraph.addEdge(StateGraph.START, "node1");
+        stateGraph.addEdge("node1", "node2");
+        stateGraph.addEdge("node2", StateGraph.END);
+
+        return stateGraph.compile();
+    }
+}
---
+++ graph/src/main/java/com/tenny/controller/GraphController.java
@@ -0,0 +1,33 @@
+package com.tenny.controller;
+
+import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("graph")
+public class GraphController {
+
+    private static final Logger log = LoggerFactory.getLogger(GraphController.class);
+    private final CompiledGraph compiledGraph;
+
+    public GraphController(CompiledGraph compiledGraph) {
+        this.compiledGraph = compiledGraph;
+    }
+
+    @GetMapping("quickStartGraph")
+    public String quickStartGraph() {
+        Optional<OverAllState> optionalOverAllState = compiledGraph.invoke(Map.of("input3", 3));
+        log.info("optionalOverAllState: {}", optionalOverAllState);
+        return "ok";
+    }
+
+}
